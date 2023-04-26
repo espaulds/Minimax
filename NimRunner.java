501bf0d2a21e217dd5dfbd48c515f9332c07cffc
espaulds <112963551+espaulds@users.noreply.github.com>
2023-04-26 13:42:04 -0400

    trying to code regular nim (not quite working/understanding :( but will keep trying

--- NimRunner.java
+++ NimRunner.java
@@ -9,7 +9,7 @@ public class NimRunner {
     }
 
     //runs game of simple nim game
-    public static boolean runGame(int state, boolean turn){
+    public static boolean runGame(ArrayList<Integer> state, boolean turn){
         //random state number
         //int state = 10;
         //runs loop while state is more than 0
@@ -33,51 +33,53 @@ public class NimRunner {
     }
 
     //move methods --> hard coded to be 1 for both
-    public static int getXMove(int state){
+    public static ArrayList<Integer> getXMove(ArrayList<Integer> state){
         return 1;
     }
 
-    public static int getYMove(int state){
+    public static ArrayList<Integer> getYMove(ArrayList<Integer> state){
         return 1;
     }
 
     //tells if state is good or bad for player
-    public static int minimax(int numPiecesRemaining, boolean myTurn){
+    public static int minimax(ArrayList<Integer> numPiecesRemaining, boolean myTurn){
         //base case --> getting best outcome scores
-        if(numPiecesRemaining == 0){
-            if(myTurn == true){
-                //good for player one
-                return 1;
-            } else {
-                //bad for player one
-                return -1;
-            }
-        //recursive step
-        } else { 
-            //arraylist to hold all possible scores in the turn
-            ArrayList<Integer> scores = new ArrayList<>();
-            //for loop going through all 3 possible moves
-            for(int numPiecesToTake = 1; numPiecesToTake<4; numPiecesToTake++){
-                //if that move can be made (you can take that number of pieces)...
-                if(numPiecesRemaining >= numPiecesToTake){
-                    //...and add best outcome scores to score
-                        //runnning minimax recursively again to get that information in base case
-                    scores.add(minimax(numPiecesRemaining-numPiecesToTake, !myTurn));
+        for(int i = 0; i<numPiecesRemaining.size(); i++){
+            if(numPiecesRemaining.get(i) == 0){
+                if(myTurn == true){
+                    //good for player one
+                    return 1;
+                } else {
+                    //bad for player one
+                    return -1;
                 }
-            }
-            //if it is your turn...
-            if(myTurn == true){
-                //...return the HIGHEST (1) of the array
-                return Collections.max(scores); //return max of array
-            } else {
-                //...return the LOWEST (-1) of the array
-                return Collections.min(scores); //return min of array
+        //recursive step
+            } else { 
+                //arraylist to hold all possible scores in the turn
+                ArrayList<Integer> scores = new ArrayList<>();
+                //for loop going through all 3 possible moves
+                for(int numPiecesToTake = 1; numPiecesToTake<numPiecesRemaining.size(); numPiecesToTake++){
+                    //if that move can be made (you can take that number of pieces)...
+                    if(numPiecesRemaining.get(i) >= numPiecesToTake){
+                        //...and add best outcome scores to score
+                            //runnning minimax recursively again to get that information in base case
+                        scores.add(minimax(numPiecesRemaining.get(i)-numPiecesToTake, !myTurn));
+                    }
+                }
+                //if it is your turn...
+                if(myTurn == true){
+                    //...return the HIGHEST (1) of the array
+                    return Collections.max(scores); //return max of array
+                } else {
+                    //...return the LOWEST (-1) of the array
+                    return Collections.min(scores); //return min of array
+                }
             }
         }
     }
 
     //gets the best possible move for the player
-    public static int bestMove(int numPiecesRemaining, boolean myTurn){
+    public static int bestMove(ArrayList<Integer> numPiecesRemaining, boolean myTurn){
         //for the number of possible moves (taking 1-3)
         for(int numPiecesToTake = 1; numPiecesToTake<4; numPiecesToTake++){
             //delcaring varible for minimax (to see if move is good or bad)
@@ -99,6 +101,20 @@ public class NimRunner {
         }
         //if a best move doesn't exist (1), then just do a random move (1-3)
         return (int) Math.random()*3+1;
+
+
+        //regular nim attempt code for getting possible moves
+        public static ArrayList<ArrayList<Integer>> getPossMoves(ArrayList<Integer> state, boolean turn){
+            ArrayList<ArrayList<Integer>> numMoves = new ArrayList<ArrayList<>>();
+            for(int i = 0; i<state.size(); i++){
+                for(int i = j; j<state.get(i); j++){
+                    ArrayList<Integer> oneMove = new ArrayList<>(Arrays.asList(0,0,0));
+                    oneMove.set(i,j);
+                }
+               numMoves.add(oneMove); 
+            }
+            return numMoves;
+        }
     }
 
     
