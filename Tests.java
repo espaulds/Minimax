e26c926a51dcdf07ae8de41915b05c90dd60baf5
espaulds <112963551+espaulds@users.noreply.github.com>
2023-04-26 13:28:02 -0400

    all tests + edge cases

--- Tests.java
+++ Tests.java
@@ -3,23 +3,42 @@ public class Tests {
     public static void main(String[] args){
         System.out.println("testing my NimRunner class:");
         System.out.println("testing runGame()");
-        System.out.println(NimRunner.runGame(1, true)); //should be false (Y win)
-        System.out.println(NimRunner.runGame(10, true)); //should be true (X win)
-        System.out.println(NimRunner.runGame(1, false)); //should be true (X win)
-        System.out.println(NimRunner.runGame(10, false)); //should be false (Y win)
+        System.out.println(NimRunner.runGame(1, true) == false); //should be false (Y win)
+        System.out.println(NimRunner.runGame(10, true) == true); //should be true (X win)
+        System.out.println(NimRunner.runGame(1, false) == true); //should be true (X win)
+        System.out.println(NimRunner.runGame(10, false) == false); //should be false (Y win)
 
-        //not working hmmmm
         System.out.println("testing minimax:");
-        System.out.println(NimRunner.minimax(1, true)); //should be 1
-        System.out.println(NimRunner.minimax(2, true)); //should be 1
-        System.out.println(NimRunner.minimax(3, true)); //should be 1
-        System.out.println(NimRunner.minimax(4, true)); //should be -1
-        System.out.println(NimRunner.minimax(5, true)); //should be -1
-        System.out.println(NimRunner.minimax(6, true)); //should be -1
-        System.out.println(NimRunner.minimax(7, true)); //should be 1
+        System.out.println(NimRunner.minimax(1, true) == -1); //should be -1
+        System.out.println(NimRunner.minimax(2, false) == -1); //should be -1
+        System.out.println(NimRunner.minimax(5, false) == 1); //should be 1
+        System.out.println(NimRunner.minimax(6, true) == 1); //should be 1
+        System.out.println(NimRunner.minimax(3, false) == -1); //should be -1
+        System.out.println(NimRunner.minimax(1, false) == 1); //should be 1
+
 
         System.out.println("testing best move:");
+        System.out.println(NimRunner.bestMove(6, true) == 1); //should be 1 (to get to 5 pieces for their move)
+        System.out.println(NimRunner.bestMove(3, true) == 2); //should be 2
+        System.out.println(NimRunner.bestMove(6, false) == 1); //should be 1
+        System.out.println(NimRunner.bestMove(4, false) == 3); //should be 3
+        System.out.println(NimRunner.bestMove(5, false)); //should be random (1)
+        System.out.println(NimRunner.bestMove(3, false) == 2); //should be 2
         
+        //edge test cases
+        System.out.println("edge test cases:");
+        System.out.println(NimRunner.runGame(0, true) == true); //should be true, no pieces to take
+        System.out.println(NimRunner.runGame(100, true) == true); //should be true (X win)
+        System.out.println(NimRunner.minimax(0, true) == 1); //should be 1
+        System.out.println(NimRunner.minimax(0, false) == -1); //should be -1
+        System.out.println(NimRunner.bestMove(0, true)); //should be random (1)
+        System.out.println(NimRunner.bestMove(0, false)); //should be random (1)
+        System.out.println(NimRunner.bestMove(1, true)); //should be random (1)
+        System.out.println(NimRunner.bestMove(1, false)); //should be random (1)
+        System.out.println(NimRunner.bestMove(2, true) == 1); //should be 1
+        System.out.println(NimRunner.bestMove(2, false) == 1); //should be 1
+        System.out.println(NimRunner.bestMove(8, true) == 3); //should be random 3
+        System.out.println(NimRunner.bestMove(8, false) == 3); //should be random 3
 
     }
 
